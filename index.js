c8cb28d961c09b32c07f8e12f893a02a517fe8d2
Minna Fingerhood <minnaf@sas.upenn.edu>
2019-03-09 02:52:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function sayHiToGrandma (string) {
     return "YES INDEED!";
   }
   
- else if (string === "I love you, grandma") {
+ else if (string === "I love you, Grandma.") {
     return "I love you, too.";
   }
   
