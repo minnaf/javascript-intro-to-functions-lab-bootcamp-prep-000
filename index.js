2200d69e44fa90d67b02ec0e213bde96f294d05c
Minna Fingerhood <minnaf@sas.upenn.edu>
2019-03-09 02:17:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function logWhisper(string){
 
 function sayHiToGrandma (string) {
   if (string.toLowerCase()) {
-    return "I can't hear you"
+    return "I can/'t hear you"
   }
   
   if (string.toUpperCase()) {
