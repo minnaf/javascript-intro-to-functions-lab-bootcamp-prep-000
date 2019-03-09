bb4d14b42c9cd826334dd3aef00cef31d71993e7
Minna Fingerhood <minnaf@sas.upenn.edu>
2019-03-09 02:10:47 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+  return string.toUpperCase()
+  
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+
+
+function logShout(string){
+  var hello = hello
+  console.log(hello.toUpperCase())
+}
+
+
+
